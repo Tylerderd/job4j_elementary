5f487b9a871347bbe2892fe13bc125863090b4bc
Tylerderd <simpskill38@gmail.com>
2020-12-16 21:33:22 +0300

    6.2.2. Переставить элементы массива [#428937]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -24,7 +24,7 @@ public class SwitchArray {
             System.out.println(value);
         }
         System.out.println();
-        int[] swapTestArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] swapTestArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] swapRes = swap(swapTestArray, 2, 4);
         for (int i : swapRes) {
             System.out.println(i);
--- test/ru/job4j/array/SwitchArrayTest.java
+++ test/ru/job4j/array/SwitchArrayTest.java
@@ -8,16 +8,16 @@ public class SwitchArrayTest {
 
     @Test
     public void whenSwap0to3() {
-        int[] input = {1, 2, 3, 4};
-        int[] expect = {4, 2, 3, 1};
+        int[] input = {1, 2, 4, 4};
+        int[] expect = {4, 2, 4, 1};
         int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
         assertThat(rsl, is(expect));
     }
 
     @Test
     public void whenSwap2to4() {
-        int[] input = {1, 2, 3, 4, 7, 44};
-        int[] expect = {1, 2, 7, 4, 3, 44};
+        int[] input = {1, 2, 3, 4, 8, 44};
+        int[] expect = {1, 2, 8, 4, 3, 44};
         int[] rsl = SwitchArray.swap(input, 2, 4);
         assertThat(rsl, is(expect));
     }
