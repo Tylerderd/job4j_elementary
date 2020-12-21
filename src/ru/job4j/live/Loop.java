c51ef1881143d9a035f8a1a3952eac1fa9cdd7a1
Tylerderd <simpskill38@gmail.com>
2020-12-21 16:57:18 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#428919]

--- src/ru/job4j/live/Loop.java
+++ src/ru/job4j/live/Loop.java
@@ -2,8 +2,12 @@ package ru.job4j.live;
 
 public class Loop {
     public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i);
+        int[] arr = {6, 7, 5, 4, 33, 32, 1, 0};
+        int i = 0;
+        do {
+            System.out.print(arr[i] + " ");
+            i++;
         }
+        while (i < arr.length);
     }
 }
