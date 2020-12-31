97d268f6df2960278550d9f6db2e773161b65f6a
Tylerderd <simpskill38@gmail.com>
2020-12-31 20:25:26 +0300

    4. Расстояние между точками в трехмерном пространстве. [#429109]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -9,19 +9,40 @@ public class Point {
 
     private int y;
 
+    private int z;
+
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
+
+        Point k = new Point(0, 0, 2);
+        Point m = new Point(0, 0, 6);
         double dist = a.distance(b);
+        double dist3d = k.distance3d(m);
         System.out.println(dist);
+        System.out.println(dist3d);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -14,4 +14,22 @@ public class PointTest {
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void when002And006then4() {
+        int expected = 4;
+        Point m = new Point(0, 0, 2);
+        Point n = new Point(0, 0, 6);
+        double out = m.distance3d(n);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when340And000then5() {
+        int expected = 5;
+        Point m = new Point(3, 4, 0);
+        Point n = new Point(0, 0, 0);
+        double out = m.distance3d(n);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
